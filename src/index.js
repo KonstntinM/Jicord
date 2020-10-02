cd1578a9a7d307154920778f3f7975e2090e5b05
Konstantin Marx <k_marx@gmx.de>
2020-10-02 12:03:56 +0200

    set up express app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+var app = express()
+
+app.get("/", function(req, res) {
+    res.json("Hello World from Express!");
+});
+  
+const port = config.Server[0].PORT || 80
+app.listen(port, () => {
+    console.log("\x1b[32m" + "The backend server started successfully and listens to port " + "\x1b[33m" + port + "\x1b[32m" + "." + "\x1b[0m")
+});
